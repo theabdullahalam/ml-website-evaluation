0a4ed1a772e7f58a429289c79dec23b5f3077b92
Abdullah Alam <abdullahalam@protonmail.com>
2023-07-18 09:48:33 -0400

    train model

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,5 @@
+import joblib
 import random
 
 def get_calculated_rating(review_content):
-    # this function will be replaced by 
-    # the ml algorithm guessing the rating
-    # based on the review
     return random.randint(1, 5)
